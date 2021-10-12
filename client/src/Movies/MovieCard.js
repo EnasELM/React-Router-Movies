279ca302ed02ea664c3a10196b5e02dd434069ea
Enas <enaselmahdwi@gmail.com>
2021-10-11 20:52:41 -0600

    Enas-Elmahdwi

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,5 +1,16 @@
 import React from 'react';
-
+import { useHistory } from 'react-router-dom';
 export default function MovieCard (props) {
-  return;
+  const history = useHistory();
+ 
+ 
+  const moviesPage = () => {
+    history.push("/movies");
+  }
+//* [ ] When a user clicks on the movie card inside `MovieList` they should be taken to `/movies/
+//id of clicked movie here}` to see the details of the selected movie.
+  return( <button
+    onClick={moviesPage}>
+    MovieList
+    </button>)
 }
